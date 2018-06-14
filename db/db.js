68f64f3b846ba6eaaba02978c5471ab0a63ebb52
adam slack <adamslack@outlook.com>
2018-06-14 18:31:58 +0100

    fleshing out db methods

--- db/db.js
+++ db/db.js
@@ -52,20 +52,88 @@ class DB {
         }
     }
 
-    async insertHostName(hostname) {
-        console.log(`Inserting Hostname: ${hostname}`);
+    async insertHostName(hostName) {
+        console.log(`Inserting Host Name: ${hostName}`);
         try {
-            const checkRows = await this.query('select * from host_names where host_name=$1', [hostname]);
+            const checkRows = await this.query('select * from host_names where host_name=$1', [hostName]);
             if (checkRows.rowCount != 0) {
-                console.log(`${hostname} already exists in the table`);
+                console.log(`${hostName} already exists in the table`);
                 return;
             }
         
-            console.log(`${hostname} not found in table, inserting it now.`);
-            await this.query('insert into host_names (host_name) values($1)', [hostname]);
+            console.log(`${hostName} not found in table, inserting it now.`);
+            await this.query('insert into host_names (host_name) values($1)', [hostName]);
         }
         catch(err) {
-            console.log(`Error inserting Hostname: ${hostname} - Error: ${err}`);
+            console.log(`Error inserting Host Name: ${hostName} - Error: ${err}`);
+        }
+    }
+
+    async insertCompanyName(companyName) {
+        console.log(`Inserting Company Name: ${companyName}`);
+        try {
+            const checkRows = await this.query('select * from companies where company_name=$1', [companyName]);
+            if (checkRows.rowCount != 0) {
+                console.log(`${companyName} already exists in the table`);
+                return;
+            }
+        
+            console.log(`${companyName} not found in table, inserting it now.`);
+            await this.query('insert into host_names (host_name) values($1)', [companyName]);
+        }
+        catch(err) {
+            console.log(`Error inserting Company Name: ${companyName} - Error: ${err}`);
+        }
+    }
+
+    async selectCompanyID(companyName) {
+        console.log(`Selecting ID for Company Name: ${companyName}`);
+        try{
+            const companyRows = await ("select id from companies where company_name = $1", [companyName]);
+            if(companyRows.rowCount == 0) {
+                return -1;
+            }
+            return companyRows[0].id;
+        }   
+        catch(err) {
+            console.log(`Error selecing ID for Company Name: ${companyName}`);
+            throw err;
+        }
+    }
+
+    async selectHostNameID(hostName) {
+        console.log(`Selecting ID for Host Name: ${hostName}`);
+        try{
+            const hostRows = await ("select id from host_names where host_name = $1", [hostName]);
+            if(hostRows.rowCount == 0) {
+                return -1;
+            }
+            return hostRows[0].id;
+        }   
+        catch(err) {
+            console.log(`Error selecing ID for Host Name: ${hostName}`);
+            throw err;
+        }
+    }
+
+
+    async insertCompanyHostPair(companyName, hostName) {
+        console.log(`Inserting Company Host Pair - Company Name: ${companyName}, Host Name: ${hostName}`);
+        try {
+            let hostID = await this.selectHostNameID(hostName);
+            if(hostID == -1) {
+                await this.insertHostName(hostName);
+                hostID = await this.selectHostNameID(hostName);
+            }
+
+            let companyID = await this.selectCompanyID(companyID);
+            if (companyID == -1) {
+                await this.insertCompanyName(companyName);
+                companyID = await this.selectCompanyID(companyName)
+            }
+        }
+        catch(err) {
+            
         }
     }
 }
